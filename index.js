80f104860cd9ddac7465986d2b81bf99be23556f
Claudemiro <dimiro1@gmail.com>
2017-12-25 17:04:15 +0100

    To string

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ exports.handler = (event, context, callback) => {
     currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
         statusCode: '200',
-        body: JSON.toString({ "time": currentTime.toString() }),
+        body: JSON.stringify({ "time": currentTime.toString() }),
     });
 };
